a9dba3a00895fb7c76bbe989677736e4c1ce3d26
defiantturtle <sarabarnes078@gmail.com>
2022-04-21 18:17:38 -0400

    Add table to display data

--- FinalProject/FinalProject/Models/HobbyContext.cs
+++ FinalProject/FinalProject/Models/HobbyContext.cs
@@ -15,7 +15,7 @@ namespace FinalProject.Models
                 new Hobby { HobbyId = 1, HobbyName = "Cooking", HobbyDescription = "Cooking for my family makes me happy" },
                 new Hobby { HobbyId = 2, HobbyName = "Swimming", HobbyDescription = "Swimming in water makes me feel relax" },
                 new Hobby { HobbyId = 3, HobbyName = "Video Games", HobbyDescription = "Playing with my friends" },
-                new Hobby { HobbyId = 4, HobbyName = "Art Journaling", HobbyDescription = "Creating art in my journal allows me to express myself"});
+                new Hobby { HobbyId = 4, HobbyName = "Art Journal", HobbyDescription = "Creating in my art journal allows me to express myself" });
         }
     }
 }
