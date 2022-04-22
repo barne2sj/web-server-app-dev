37ecb256aa1c21b4e02f6514297e1fe4ef6a53f4
defiantturtle <sarabarnes078@gmail.com>
2022-04-22 14:12:50 -0400

    Added remaining hobbies to database, fixed a small issue with table

---
+++ FinalProject/FinalProject/Migrations/20220422180915_RemainingHobbies.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinalProject.Migrations
+{
+    public partial class RemainingHobbies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Hobbys",
+                columns: new[] { "HobbyId", "HobbyDescription", "HobbyName" },
+                values: new object[] { 5, "Taking photos of random things such as buildings.", "Photography" });
+
+            migrationBuilder.InsertData(
+                table: "Hobbys",
+                columns: new[] { "HobbyId", "HobbyDescription", "HobbyName" },
+                values: new object[] { 6, "Watching and analyzing movies while having meaningful discussions.", "Films" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hobbys",
+                keyColumn: "HobbyId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Hobbys",
+                keyColumn: "HobbyId",
+                keyValue: 6);
+        }
+    }
+}
